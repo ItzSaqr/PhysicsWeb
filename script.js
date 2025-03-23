75a2675a9d7914a37ab2669af4db5f2228c81f0f
ItzSaqr <yrik435@mail.ru>
2025-03-23 15:10:45 +0700

    123

--- script.js
+++ script.js
@@ -310,4 +310,21 @@ function setupEventListeners() {
         isDrawing = false;
     });
 
+            document.getElementById('themeToggle').addEventListener('click', function() {
+            document.body.classList.toggle('light-theme');
+            const icon = this.querySelector('i');
+            if (document.body.classList.contains('light-theme')) {
+                icon.classList.remove('fa-sun');
+                icon.classList.add('fa-moon');
+            } else {
+                icon.classList.remove('fa-moon');
+                icon.classList.add('fa-sun');
+            }
+        });
+
+        document.getElementById('languageSelect').addEventListener('change', function() {
+            const lang = this.value;
+            console.log('Язык изменен на: ' + lang);
+            // Здесь можно добавить логику перевода
+        });
 }
